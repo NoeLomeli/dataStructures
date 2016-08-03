9d808bd93c44c34736e07587a7d56ed12cfa3cf2
Noe Lomeli <noelomeli@student.hartnell.edu>
2016-08-02 17:37:27 -0700

    Add driver file and instructions function

---
+++ driver.cpp
@@ -0,0 +1,19 @@
+// Noé Lomelí
+// Linked list program allow user to add new nodes at the front or back of the list
+// August 02, 2016
+
+#include <iostream>
+#include <string>
+#inlcude "List.h"
+using namespace std;
+
+// display program instructions to user
+void instructions()
+{
+  cout << "Enter one of the following:\n"
+       << " 1 to insert at beginning of list\n"
+       << " 2 to insert at end of list\n"
+       << " 3 to delete from beginning\n"
+       << " 4 to delete from end of list\n"
+       << " 5 to end list processing\n";
+}// end instructions
