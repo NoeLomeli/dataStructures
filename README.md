9315d0297bd64bd19fcf3011f7a818ee0a4279e2
Noe Lomeli <noelomeli@student.hartnell.edu>
2016-08-03 14:03:42 -0700

    Add functions to Stack file

---
+++ README.md
@@ -0,0 +1,2 @@
+# dataStructures
+These are the data structure exercises on chapter 19 of the book "c++ How to Program" by Paul and Harvey Deitel.
--- Stack.h
+++ Stack.h
@@ -1,3 +1,38 @@
 // Noé Lomelí
 // August 3, 2016
 // Stacks class definition
+
+#ifndef STACK_H
+#define STACK_H
+
+#include "List.h"
+
+template< typename STACKTYPE >
+class Stack : private List< STACKTYPE >
+{
+public:
+  // push calls the List function insertAtFront
+  void push(const STACKTYPE &data)
+  {
+    insertAtFront(data);
+  }// end push
+
+  // pop calls the List function removeFromFront
+  bool pop( STACKTYPE &data)
+  {
+    return removeFromFront(data);
+  }// end pop
+
+  // isStackEmpty calls the function isStackEmpty
+  bool isStackEmpty() const
+  {
+    return this->isEmpty();
+  }// end isStackEmpty
+
+  // printStack calls the List function print
+  void printStack() const
+  {
+    this->print();
+  }// end printStack
+}; // end class Stack
+#endif
