a69141067aa71e1523535f23555cfc11c697d2c4
Noe Lomeli <noelomeli@student.hartnell.edu>
2016-08-03 14:57:58 -0700

    Add functions to Queue.h file

---
+++ Queue.h
@@ -0,0 +1,37 @@
+// Noé Lomelí
+// August 3, 2016
+// Queue class template
+
+#ifndef QUEUE_H
+#define QUEUE_H
+#include "List.h"
+
+template< typename QUEUETYPE >
+class Queue : private List< QUEUETYPE >
+{
+public:
+  //enqueue calls List member fuction insertAtBack
+  void enqueue(const QUEUETYPE &data)
+  {
+    insertAtBack(data);
+  }// end function enqueue
+
+  //dequeue calls List member function removeFromFront
+  bool dequeue(QUEUETYPE &data)
+  {
+    return removeFromFront(data);
+  }// end dequeue
+
+  // isQueueEmpty calls List member function isEmpty
+  bool isQueueEmpty() const
+  {
+    return this->isEmpty();
+  }// end isQueueEmpty
+
+  //printQueue calls List member function print
+  void printQueue() const
+  {
+    this->print();
+  }// end printQueue
+};
+#endif
